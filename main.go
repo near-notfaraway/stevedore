c52b65135d01c655e59d5a2d038eb72289c409cc
near-notfaraway <near.notfaraway@gmail.com>
2022-09-19 21:04:31 +0800

    flag to running as a daemon

--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 	"github.com/near-notfaraway/stevedore/sd_server"
 	"github.com/near-notfaraway/stevedore/sd_util"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
 func main() {
-	// parse option config
-	configPath := flag.String("config", "../etc/config.json", "config file path")
+	// parse option
+	daemon := flag.Bool("daemon", false, "Running as a daemon")
+	configPath := flag.String("config", "../etc/config.json", "Path to config file")
 	flag.Parse()
+	handleDaemonFlag(daemon)
 
 	// parse file config
 	var config sd_config.Config
@@ -36,4 +40,23 @@ func main() {
 
 	// listen and serve
 	panic(sd_server.NewServer(&config).ListenAndServe())
+}
+
+
+func handleDaemonFlag(daemon *bool) {
+	daemonEnv := os.Getenv("STEVEDORE_DAEMON")
+	if daemonEnv == "True" {
+		return
+	}
+
+	if *daemon {
+		cmd := exec.Command(os.Args[0], os.Args[1:]...)
+		cmd.Env = []string{"STEVEDORE_DAEMON=True"}
+		if err := cmd.Start(); err != nil {
+			fmt.Printf("start %s failed, error: %v\n", os.Args[0], err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s [PID] %d running...\n", os.Args[0], cmd.Process.Pid)
+		os.Exit(0)
+	}
 }
